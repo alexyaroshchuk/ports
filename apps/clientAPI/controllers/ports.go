0a07b96e6f918b0f3e997a26223014617c1179e0
aleksandryaroshchuk <oyaroshchuk@gbsfo.com>
2020-11-01 16:10:52 +0200

    first commit v2

--- apps/clientAPI/controllers/ports.go
+++ apps/clientAPI/controllers/ports.go
@@ -1,11 +1,9 @@
 package controllers
 
-import "context"
-
 type PortService interface {
-	GetPort(context.Context, *pb.RequestWithId) (*pb.PortResponse, error)
+
 }
 
 func GetPort()  {
-	ConvertPo
+
 }
